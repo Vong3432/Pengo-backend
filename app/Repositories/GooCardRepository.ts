d85c0f44357424e29fa87daa1acd368678bad8fb
Vong Nyuk Soon <vongnyuksoon2000@gmail.com>
2021-07-28 19:59:41 +0800

    fix(repositories): Fix return type in creating goocard

--- app/Repositories/GooCardRepository.ts
+++ app/Repositories/GooCardRepository.ts
@@ -1,11 +1,10 @@
 import GooCard from "App/Models/GooCard";
-import User from "App/Models/User";
 import { DBTransactionService } from "App/Services/DBTransactionService";
 import { ICreateGooCard } from "App/Services/goocard/IGooCard";
 
 export const gooCardRepository = {
 
-    async create(data: ICreateGooCard): Promise<User | any> {
+    async create(data: ICreateGooCard): Promise<GooCard> {
         const card = new GooCard();
         const trx = await DBTransactionService.init();
         try {
@@ -18,16 +17,4 @@ export const gooCardRepository = {
         }
     },
 
-    async update(data: any, id: Number): Promise<User> {
-        const trx = await DBTransactionService.init();
-        try {
-            const user = await User.findByOrFail('id', id);
-            await user.useTransaction(trx).merge({ ...data }).save();
-            await trx.commit();
-            return this.user;
-        } catch (error) {
-            await trx.rollback();
-            throw "Something went wrong"
-        }
-    }
 }
