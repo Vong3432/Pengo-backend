ac4a17dbf1157029835b9c83cd8d702034fc9b82
Vong3432 <vongnyuksoon2000@gmail.com>
2021-11-20 20:29:44 +0800

    test: Test stripe fix

--- app/Services/goocard/GooCardService.ts
+++ app/Services/goocard/GooCardService.ts
@@ -102,15 +102,18 @@ class GooCardService implements GoocardInterface {
 
             await user.load('goocard', q => q.preload('bankAccounts'));
 
+            console.log(user.goocard.bankAccounts)
 
             // check if current goocard has any bank accounts in DB
             if (user.goocard.bankAccounts.length === 0) {
                 // does not has any record, create a new one
                 fromCus = await StripeService.getStripe().customers.create({ description: user.username });
+                console.log('create', fromCus)
                 await BankAccountService.create(fromCus.id, BankAccountType.GOOCARD, user.goocard.id);
             } else {
                 // does has record, return it from db
                 fromCus = await StripeService.getStripe().customers.retrieve(user.goocard.bankAccounts[0].uniqueId);
+                console.log('retrive', fromCus)
             }
 
             // refresh
