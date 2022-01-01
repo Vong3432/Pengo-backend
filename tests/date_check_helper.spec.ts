c7d7737a61a73a693d4fd58521d2f620f551a48b
Vong3432 <vongnyuksoon2000@gmail.com>
2022-01-02 01:18:33 +0800

    chore: Update date_check_helper test case

--- tests/date_check_helper.spec.ts
+++ tests/date_check_helper.spec.ts
@@ -4,27 +4,27 @@ import { DateTime } from 'luxon';
 
 test.group('Testing date_check_helper module', (group) => {
 
-    group.before(async() => {
+    group.before(async () => {
         // save the current state of db before testing
         // await Database.beginGlobalTransaction()
     })
 
-    group.after(async() => {
+    group.after(async () => {
         // restore db to the state before testing
         // await Database.rollbackGlobalTransaction()
     })
 
-    test('Should return true when current is over than target (ISO format)', async(assert) => {
+    test('Should return true when current is over than target (ISO format)', async (assert) => {
         const currentIso: string = DateTime.now().toISO()
         const targetIso: string = DateTime.fromFormat('2021-01-01 00:00:00', "yyyy-MM-dd HH:mm:ss").toISO()
-        const isCurrentNewerThanTargetIso = DateCheckHelperService.isCurrentOverTargetISO({targetIso, currentIso})
+        const isCurrentNewerThanTargetIso = DateCheckHelperService.isCurrentOverTargetISO({ targetIso, currentIso })
         assert.isTrue(isCurrentNewerThanTargetIso)
     })
 
-    test('Should return false when target is over than current (ISO format)', async(assert) => {
+    test('Should return false when target is over than current (ISO format)', async (assert) => {
         const currentIso: string = DateTime.now().toISO()
-        const targetIso: string = DateTime.fromFormat('2022-01-01 00:00:00', "yyyy-MM-dd HH:mm:ss").toISO()
-        const isCurrentNewerThanTargetIso = DateCheckHelperService.isCurrentOverTargetISO({targetIso, currentIso})
+        const targetIso: string = DateTime.now().plus({ days: 1 }).toISO()
+        const isCurrentNewerThanTargetIso = DateCheckHelperService.isCurrentOverTargetISO({ targetIso, currentIso })
         assert.isFalse(isCurrentNewerThanTargetIso)
     })
 })
