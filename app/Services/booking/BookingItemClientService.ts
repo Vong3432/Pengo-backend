6220a2e4e6d434c832ea01123567df896325c9d7
Vong3432 <vongnyuksoon2000@gmail.com>
2022-01-22 03:13:26 +0800

    phaseout

--- app/Services/booking/BookingItemClientService.ts
+++ app/Services/booking/BookingItemClientService.ts
@@ -6,216 +6,230 @@ import { getDistance, orderByDistance, isPointWithinRadius } from "geolib";
 import { DateTime } from "luxon";
 
 class BookingItemClientService implements BookingItemClientInterface {
+  findAllByPengerAndCategory(): Promise<BookingItem[]> {
+    throw new Error("Method not implemented.");
+  }
 
-    findAllByPengerAndCategory(): Promise<BookingItem[]> {
-        throw new Error("Method not implemented.");
+  async findAllByPenger({ request, auth }: HttpContextContract) {
+    const user = await auth.authenticate();
+    const isLoggedIn = auth.isLoggedIn;
+
+    if (user !== null && isLoggedIn) await user.load("goocard");
+
+    const penger = await PengerService.findById(request.qs().penger_id);
+    await penger.load("bookingItems", (q) => {
+      if (isLoggedIn) {
+        q.where("goocard_id", user.goocard.id);
+      }
+    });
+
+    return penger.bookingItems;
+  }
+
+  async findAll(contract: HttpContextContract) {
+    const { request, auth } = contract;
+    const {
+      penger_id: pengerId,
+      category_id: categoryId,
+      price,
+      name,
+      lat,
+      lng,
+      km,
+      sort_date,
+      sort_distance,
+      limit,
+    } = request.qs();
+    const user = await auth.authenticate();
+    const isLoggedIn = auth.isLoggedIn;
+
+    if (user !== null && isLoggedIn) await user.load("goocard");
+
+    const q = BookingItem.query();
+
+    if (name) {
+      const trimName: string = name.toString().trim();
+      q.where("name", "like", `%${trimName}%`);
     }
 
-    async findAllByPenger({ request, auth }: HttpContextContract) {
-        const user = await auth.authenticate()
-        const isLoggedIn = auth.isLoggedIn
-
-        if (user !== null && isLoggedIn) await user.load('goocard')
-
-        const penger = await PengerService.findById(request.qs().penger_id);
-        await penger.load('bookingItems', q => {
-            if (isLoggedIn) {
-                q.where('goocard_id', user.goocard.id)
-            }
-        });
-
-        return penger.bookingItems
+    if (limit) {
+      q.limit(limit);
     }
 
-    async findAll(contract: HttpContextContract) {
-        const { request, auth } = contract;
-        const {
-            penger_id: pengerId,
-            category_id: categoryId,
-            price,
-            name,
-            lat,
-            lng,
-            km,
-            sort_date,
-            sort_distance,
-            limit,
-        } = request.qs()
-        const user = await auth.authenticate()
-        const isLoggedIn = auth.isLoggedIn
-
-        if (user !== null && isLoggedIn) await user.load('goocard')
-
-        const q = BookingItem.query()
-
-        if (name) {
-            const trimName: string = name.toString().trim()
-            q.where("name", "like", `%${trimName}%`)
-        }
-
-        if (limit) {
-            q.limit(limit)
-        }
-
-        // if findAll by Penger
-        if (pengerId) {
-            q.preload('category', q => q.where('created_by', pengerId))
-        }
-
-        if (price) {
-            if (price !== "free" && price !== null)
-                q.whereBetween('price', [0, price])
-        }
-
-        // if has categoryId
-        if (categoryId) {
-            q.where('booking_category_id', categoryId)
-        }
-
-        if (isLoggedIn) {
-            q.preload('records', recordQuery => {
-                recordQuery.where('goocard_id', user.goocard.id)
-            })
-        }
-
-        if (sort_date == 1) {
-            //sort by date
-            q.orderBy('created_at', 'desc')
-        }
-
-        if (sort_distance == 1 || (lng && lat)) {
-            //sort by distance
-            const arr = await this.getOpenItems(await q)
-            const filtered = this.sortItemListByDistance(arr, (sort_distance == 1), lat, lng, km)
-            return filtered
-        }
-
-        return await this.getOpenItems(await q);
-    };
-
-    /**
-     * 
-     * @param item 
-     * @description Return item that is open only
-     */
-    async isOpened(item: BookingItem) {
-
-        await item.load('category', q => q.preload('createdBy'))
-        const penger = item.category.createdBy
-        return await PengerService.isOpen(penger) === true
+    // if findAll by Penger
+    if (pengerId) {
+      q.preload("category", (q) => q.where("created_by", pengerId));
     }
 
-    /**
-     * 
-     * @param items 
-     * @description Return items that is open only
-     */
-    async getOpenItems(items: BookingItem[]) {
-        let opened: BookingItem[] = []
-
-        for await (const item of items) {
-            if (await this.isOpened(item)) {
-                opened.push(item)
-            }
-        }
-
-        return opened
+    if (price) {
+      if (price !== "free" && price !== null)
+        q.whereBetween("price", [0, price]);
     }
 
-    /**
-    * 
-    * @param items 
-    * @param sortDistance
-    * @param lat 
-    * @param lng 
-    * @param radius (optional, 1km = 1000r)
-    * @description lowest will be at the top
-    */
-    async sortItemListByDistance(items: BookingItem[], sortDistance: boolean, lat: number, lng: number, km?: number) {
-        const origin = { longitude: lng, latitude: lat }
-        let filteredArr = items
-
-        if (km) {
-            // if filter radius is request,
-            // filter out the items that is out of range
-            filteredArr = filteredArr.filter((p) => {
-
-                if (p.isVirtual) return false
-
-                const geoObj = JSON.parse(p.geolocation)
-                const isIn: boolean = isPointWithinRadius(
-                    origin,
-                    { ...geoObj },
-                    km * 1000 // km to radius
-                );
-                console.log(`Distance(km): ${getDistance(origin, geoObj) / 1000}, radius: ${km * 1000}, inPoint: ${isIn}`)
-                return isIn
-            })
-        }
-
-        if (filteredArr.length === 0) return []
-
-        if (sortDistance) {
-            const sortedArr = orderByDistance(origin, items
-                .filter((p) => p.isVirtual !== 1)
-                .map((p) => {
-                    const geoObj = JSON.parse(p.geolocation)
-                    const { latitude, longitude } = geoObj
-                    return {
-                        ...p.serialize(),
-                        latitude,
-                        longitude,
-                        distance: getDistance(origin, geoObj) / 1000 // m to km
-                    }
-                }))
-
-            // return items filter w/ radius + sorted distance
-            return sortedArr;
-        }
-
-        // return items filter with radius only
-        return filteredArr
+    // if has categoryId
+    if (categoryId) {
+      q.where("booking_category_id", categoryId);
     }
 
-    async findById({ request, auth }: HttpContextContract) {
-        const id = request.param('id')
-        const user = await auth.authenticate()
-        const isLoggedIn = auth.isLoggedIn
-        const bookingItem = await BookingItem.findOrFail(id)
+    if (isLoggedIn) {
+      q.preload("records", (recordQuery) => {
+        recordQuery.where("goocard_id", user.goocard.id);
+      });
+    }
 
-        if (user !== null && isLoggedIn) {
-            await user.load('goocard')
+    if (sort_date == 1) {
+      //sort by date
+      q.orderBy("created_at", "desc");
+    }
 
-            await user.goocard
-                .load('coupons', q => {
-                    q.where('valid_to', '>', DateTime.now().toISO())
-                    q.where('is_used', 0)
-                });
+    if (sort_distance == 1 || (lng && lat)) {
+      //sort by distance
+      const arr = await this.getOpenItems(await q);
+      const filtered = this.sortItemListByDistance(
+        arr,
+        sort_distance == 1,
+        lat,
+        lng,
+        km
+      );
+      return filtered;
+    }
 
-            const availableCouponIds = user.goocard.coupons.map(c => c.id)
+    return await this.getOpenItems(await q);
+  }
 
-            bookingItem.load('coupons', q => {
-                q.whereIn('coupons.id', availableCouponIds)
-            })
-        }
+  /**
+   *
+   * @param item
+   * @description Return item that is open only
+   */
+  async isOpened(item: BookingItem) {
+    await item.load("category", (q) => q.preload("createdBy"));
+    const penger = item.category.createdBy;
+    return (await PengerService.isOpen(penger)) === true;
+  }
 
-        await bookingItem.load('category', q => {
-            q.preload('createdBy', pengerQ => {
-                pengerQ.preload('location')
-                pengerQ.preload('closeDates')
-            })
-            q.preload('bookingOptions', optionQ => optionQ.where('is_active', 1))
-        });
+  /**
+   *
+   * @param items
+   * @description Return items that is open only
+   */
+  async getOpenItems(items: BookingItem[]) {
+    let opened: BookingItem[] = [];
 
-        await bookingItem.load('records', q => {
-            if (isLoggedIn) {
-                q.where('goocard_id', user.goocard.id)
-            }
-        })
+    for await (const item of items) {
+      if (await this.isOpened(item)) {
+        opened.push(item);
+      }
+    }
 
-        return bookingItem
-    };
+    return opened;
+  }
 
+  /**
+   *
+   * @param items
+   * @param sortDistance
+   * @param lat
+   * @param lng
+   * @param radius (optional, 1km = 1000r)
+   * @description lowest will be at the top
+   */
+  async sortItemListByDistance(
+    items: BookingItem[],
+    sortDistance: boolean,
+    lat: number,
+    lng: number,
+    km?: number
+  ) {
+    const origin = { longitude: lng, latitude: lat };
+    let filteredArr = items;
+
+    if (km) {
+      // if filter radius is request,
+      // filter out the items that is out of range
+      filteredArr = filteredArr.filter((p) => {
+        if (p.isVirtual && p.geolocation === null) return false;
+
+        const geoObj = JSON.parse(p.geolocation);
+        const isIn: boolean = isPointWithinRadius(
+          origin,
+          { ...geoObj },
+          km * 1000 // km to radius
+        );
+        console.log(
+          `Distance(km): ${getDistance(origin, geoObj) / 1000}, radius: ${
+            km * 1000
+          }, inPoint: ${isIn}`
+        );
+        return isIn;
+      });
+    }
+
+    if (filteredArr.length === 0) return [];
+
+    if (sortDistance) {
+      const sortedArr = orderByDistance(
+        origin,
+        items
+          .filter((p) => p.isVirtual !== 1 || p.geolocation)
+          .map((p) => {
+            const geoObj = JSON.parse(p.geolocation);
+            const { latitude, longitude } = geoObj;
+            return {
+              ...p.serialize(),
+              latitude,
+              longitude,
+              distance: getDistance(origin, geoObj) / 1000, // m to km
+            };
+          })
+      );
+
+      // return items filter w/ radius + sorted distance
+      return sortedArr;
+    }
+
+    // return items filter with radius only
+    return filteredArr;
+  }
+
+  async findById({ request, auth }: HttpContextContract) {
+    const id = request.param("id");
+    const user = await auth.authenticate();
+    const isLoggedIn = auth.isLoggedIn;
+    const bookingItem = await BookingItem.findOrFail(id);
+
+    if (user !== null && isLoggedIn) {
+      await user.load("goocard");
+
+      await user.goocard.load("coupons", (q) => {
+        q.where("valid_to", ">", DateTime.now().toISO());
+        q.where("is_used", 0);
+      });
+
+      const availableCouponIds = user.goocard.coupons.map((c) => c.id);
+
+      bookingItem.load("coupons", (q) => {
+        q.whereIn("coupons.id", availableCouponIds);
+      });
+    }
+
+    await bookingItem.load("category", (q) => {
+      q.preload("createdBy", (pengerQ) => {
+        pengerQ.preload("location");
+        pengerQ.preload("closeDates");
+      });
+      q.preload("bookingOptions", (optionQ) => optionQ.where("is_active", 1));
+    });
+
+    await bookingItem.load("records", (q) => {
+      if (isLoggedIn) {
+        q.where("goocard_id", user.goocard.id);
+      }
+    });
+
+    return bookingItem;
+  }
 }
 
 export default new BookingItemClientService();
--- app/Validators/penger/UpdateCouponValidator.ts
+++ app/Validators/penger/UpdateCouponValidator.ts
@@ -1,72 +1,73 @@
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { MyReporter } from '../MyReporter'
+import { schema, rules } from "@ioc:Adonis/Core/Validator";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { MyReporter } from "../MyReporter";
 
 export default class UpdateCouponValidator {
-	constructor(protected ctx: HttpContextContract) {
-	}
+  constructor(protected ctx: HttpContextContract) {}
 
-	public reporter = MyReporter
+  public reporter = MyReporter;
 
-	/*
-	 * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-	 *
-	 * For example:
-	 * 1. The username must be of data type string. But then also, it should
-	 *    not contain special characters or numbers.
-	 *    ```
-	 *     schema.string({}, [ rules.alpha() ])
-	 *    ```
-	 *
-	 * 2. The email must be of data type string, formatted as a valid
-	 *    email. But also, not used by any other user.
-	 *    ```
-	 *     schema.string({}, [
-	 *       rules.email(),
-	 *       rules.unique({ table: 'users', column: 'email' }),
-	 *     ])
-	 *    ```
-	 */
-	public schema = schema.create({
-		title: schema.string.optional(),
-		description: schema.string.optional(),
-		min_credit_points: schema.number.optional([
-			rules.range(0, 5000)
-		]),
-		required_credit_points: schema.number.optional([
-			rules.range(0, 5000)
-		]),
-		valid_from: schema.date.optional({ format: 'yyyy-MM-dd HH:mm:ss' }, [
-			// rules.after('today')
-		]),
-		valid_to: schema.date.optional({ format: 'yyyy-MM-dd HH:mm:ss' }, [rules.after('today')]),
-		quantity: schema.number.optional([rules.unsigned()]),
-		is_redeemable: schema.boolean.optional(),
-		is_scannable: schema.boolean.optional(),
-		is_selectable: schema.boolean.optional(),
-		discount_percentage: schema.number.optional(),
-		// discount_amount: schema.number.optional(),
-		only_to_items: schema.array.optional().members(
-			schema.number([
-				rules.exists({ table: 'booking_items', column: 'id' })
-			])),
-	})
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    title: schema.string.optional(),
+    description: schema.string.optional(),
+    min_credit_points: schema.number.optional([rules.range(0, 5000)]),
+    required_credit_points: schema.number.optional([rules.range(0, 5000)]),
+    valid_from: schema.date.optional({ format: "yyyy-MM-dd HH:mm:ss" }, [
+      // rules.after('today')
+    ]),
+    valid_to: schema.date.optional({ format: "yyyy-MM-dd HH:mm:ss" }, [
+      // rules.after('today')
+    ]),
+    quantity: schema.number.optional([rules.unsigned()]),
+    is_redeemable: schema.boolean.optional(),
+    is_scannable: schema.boolean.optional(),
+    is_selectable: schema.boolean.optional(),
+    discount_percentage: schema.number.optional(),
+    // discount_amount: schema.number.optional(),
+    only_to_items: schema.array
+      .optional()
+      .members(
+        schema.number([rules.exists({ table: "booking_items", column: "id" })])
+      ),
+  });
 
-	/**
-	 * Custom messages for validation failures. You can make use of dot notation `(.)`
-	 * for targeting nested fields and array expressions `(*)` for targeting all
-	 * children of an array. For example:
-	 *
-	 * {
-	 *   'profile.username.required': 'Username is required',
-	 *   'scores.*.number': 'Define scores as valid numbers'
-	 * }
-	 *
-	 */
-	public messages = {
-		'valid_from.after': 'Start at date is not valid, should be after current time.',
-		'min_credit_points.range': 'Min. credit points should range between 0-5000.',
-		'required_credit_points.range': 'Required credit points should range between 0-5000.',
-		'only_to_items.array': 'Invalid items',
-	}
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages = {
+    "valid_from.after":
+      "Start at date is not valid, should be after current time.",
+    "min_credit_points.range":
+      "Min. credit points should range between 0-5000.",
+    "required_credit_points.range":
+      "Required credit points should range between 0-5000.",
+    "only_to_items.array": "Invalid items",
+  };
 }
